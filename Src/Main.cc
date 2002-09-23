e5a2312e52d8a96d37c91370732c426f27c2839d
pjc51 <pjc51>
2002-09-23 21:14:46 +0000

    Committing new stuff

---
+++ Src/Main.cc
@@ -0,0 +1,19 @@
+// Gtk includes
+
+#include <gtk--/main.h>
+
+// Gtk Dasher includes
+
+#include "GtkDasherWindow.h"
+
+int main( int argc, char **argv )
+{
+  Gtk::Main gtk_main( argc, argv );
+
+  GtkDasherWindow main_window;
+  main_window.show();
+
+  gtk_main.run();
+
+  return( 0 );
+}
