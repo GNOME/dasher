faa41cc72e0b369f7a82542877b676e54f032715
mjg59 <mjg59>
2003-03-12 00:14:06 +0000

    Add support for the objects colour scheme

--- Src/Win32/Widgets/Screen.cpp
+++ Src/Win32/Widgets/Screen.cpp
@@ -118,12 +118,14 @@ FontSize CScreen::GetFontSize()
 
 void CScreen::Build_Colours ()
 {
-	m_Brushes.resize(5);
+	m_Brushes.resize(6);
 	
 	// Currently white and gray. Intended for use by a space character, placed last in alphabet
 	m_Brushes[Special1].push_back(CreateSolidBrush(RGB(240,240,240))); // making lighter for djcm
 	m_Brushes[Special2].push_back(CreateSolidBrush(RGB(255,255,255)));
 	
+	m_Brushes[Objects].push_back(CreateSolidBrush(RGB(0,0,0)));
+
 	m_Brushes[Groups].push_back(CreateSolidBrush(RGB(255,255,0)));
 	m_Brushes[Groups].push_back(CreateSolidBrush(RGB(0,255,0)));
 	m_Brushes[Groups].push_back(CreateSolidBrush(RGB(255,100,100)));
