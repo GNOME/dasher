95f7a75d930dc5d7e922a343975faa28e16792cb
mjg59 <mjg59>
2003-11-12 18:30:51 +0000

    We need to call ChangeLanguageModel earlier due to the changes in DasherInterface

--- Src/Win32/WinMain.cpp
+++ Src/Win32/WinMain.cpp
@@ -116,9 +116,9 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 	DasherInterface.ColourMode(true);
 
 	{
+		DasherInterface.ChangeLanguageModel(0);
 		CDasherWindow DasherWindow(&DasherInterface, &DasherInterface, &DasherInterface); // Main Window
 		DasherInterface.SetSettingsUI(&DasherWindow);         // The UI will be updated to reflect settings
-		DasherInterface.ChangeLanguageModel(0);
 		DasherInterface.AddControlTree(WinMenus::GetWindowMenus()); // Build control tree
 		DasherWindow.Show(nCmdShow);                          // Now set up. Kill splash screen and display main window
 
