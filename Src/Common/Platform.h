d80d37d8ead9e978a2e77564c8fc1ff434f788f5
David Ward <dward@src.gnome.org>
2004-10-17 21:59:13 +0000

    New platform-detection file

---
+++ Src/Common/Platform.h
@@ -0,0 +1,30 @@
+// Platform.h
+//
+/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2004 David Ward
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef __Platform_h__
+#define __Platform__
+
+// Place to set platform-defines of the form DASHER_........
+
+#ifdef _MSC_VER
+
+	#ifdef WIN32
+		#define DASHER_WIN32
+	#endif
+
+	#ifdef _WIN32_WCE
+		#define DASHER_WINCE
+	#endif
+
+#else
+	
+	// Please set platform-defines here
+
+#endif
+
+#endif // __include__
