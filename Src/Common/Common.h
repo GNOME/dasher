bdb36082847e2946f9fbbff01c79fa8d0aed072d
David Ward <dward@src.gnome.org>
2004-10-31 11:45:46 +0000

    add missing common.h

---
+++ Src/Common/Common.h
@@ -0,0 +1,23 @@
+// Common.h
+//
+/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2004 David Ward
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef __Common_h__
+#define __Common_h__
+
+// Place common includes here - to be include by every .cpp file before its header 
+//		- can be used for precompiled headers
+//		- also when order of headers is important
+
+#include "Platform.h"
+
+	#ifdef DASHER_WIN32
+		#include "MSVC_Unannoy.h"
+	#endif
+	
+
+#endif // __include__
