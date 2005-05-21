cadd954e2c5a3821f200eaf46e807daccb0b5e17
David Ward <dward@src.gnome.org>
2005-05-21 12:53:15 +0000

    create alphabet dir. remove customalphabet and put functionality in CAlphabet, which neednt be a base class

---
+++ Src/DasherCore/Alphabet/Alphabet.cpp
@@ -0,0 +1,267 @@
+// Alphabet.cpp
+//
+/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2001-2005 David Ward
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#include "../../Common/Common.h"
+
+
+#include "Alphabet.h"
+#include "AlphabetMap.h"
+
+using namespace Dasher;
+using namespace std;
+
+/////////////////////////////////////////////////////////////////////////////
+
+
+CAlphabet::CAlphabet()
+	: m_DefaultEncoding(Opts::Western), m_Orientation(Opts::LeftToRight), 
+	m_ControlSymbol(-1), m_iGroups(0)
+{
+	m_Characters.push_back("");
+	m_Display.push_back("");
+	m_Colours.push_back(-1);
+	m_Foreground.push_back("");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+CAlphabet::CAlphabet(const CAlphIO::AlphInfo& AlphInfo)
+	: m_DefaultEncoding(Opts::Western), m_Orientation(Opts::LeftToRight), 
+	m_ControlSymbol(-1), m_iGroups(0)
+{
+	m_Characters.push_back("");
+	m_Display.push_back("");
+	m_Colours.push_back(-1);
+	m_Foreground.push_back("");
+
+	// Set miscellaneous options
+	SetOrientation(AlphInfo.Orientation);
+	SetLanguage(AlphInfo.Type);
+	SetTrainingFile(AlphInfo.TrainingFile);
+	SetPalette(AlphInfo.PreferredColours);
+	
+//	m_AlphInfo=&AlphInfo;
+
+	// Add all the characters.
+	for (unsigned int i=0; i<AlphInfo.Groups.size(); i++) { // loop groups
+		CGroupAdder* pAdder = GetGroupAdder(AlphInfo.Groups[i].Colour, AlphInfo.Groups[i].Label);
+        for (unsigned int j=0; j<AlphInfo.Groups[i].Characters.size(); j++) { // loop characters
+			pAdder->AddChar(AlphInfo.Groups[i].Characters[j].Text, AlphInfo.Groups[i].Characters[j].Display, AlphInfo.Groups[i].Characters[j].Colour, AlphInfo.Groups[i].Characters[j].Foreground);
+		}
+		delete pAdder;
+	}
+	
+	// Set Space character if requested
+
+	// This line makes it a bit easier for our WindowsCE compiler
+	std::string empty="";
+
+	if (AlphInfo.ParagraphCharacter.Text != empty )
+		AddParagraphSymbol(AlphInfo.ParagraphCharacter.Text, AlphInfo.ParagraphCharacter.Display, AlphInfo.ParagraphCharacter.Colour, AlphInfo.ParagraphCharacter.Foreground);
+
+
+	if (AlphInfo.SpaceCharacter.Text != empty ) 
+		AddSpaceSymbol(AlphInfo.SpaceCharacter.Text, AlphInfo.SpaceCharacter.Display, AlphInfo.SpaceCharacter.Colour, AlphInfo.SpaceCharacter.Foreground);
+
+	// DJW - now the control symbol is always a part of the alphabet
+	// DasherModel knows whether or not to use it
+	if (AlphInfo.ControlCharacter.Display != std::string("") && GetControlSymbol()==-1) 
+		AddControlSymbol(AlphInfo.ControlCharacter.Text, AlphInfo.ControlCharacter.Display, AlphInfo.ControlCharacter.Colour, AlphInfo.ControlCharacter.Foreground);
+
+#ifdef DASHER_TRACE
+	Trace();
+#endif
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+void CAlphabet::GetSymbols(vector<symbol>* Symbols, string* Input, bool IsMore) const
+{
+	string Tmp;
+	symbol CurSymbol=0, TmpSymbol=0;
+	bool KeyIsPrefix=false;
+	int extras;
+	unsigned int bit;
+
+	for (unsigned int i=0; i<Input->size(); i++) {
+
+		Tmp = (*Input)[i];
+
+		/* The string we've been given is in UTF-8. The symbols are
+		   also in UTF-8, so we need to pass the entire UTF-8 character
+		   which may be several bytes long. RFC 2279 describes this
+		   encoding */
+
+		if ((*Input)[i] & 0x80) { // Character is more than 1 byte long
+		  extras = 1;
+		  for (bit = 0x20; ((*Input)[i] & bit) != 0; bit >>= 1)
+		    extras++;
+		  if (extras > 5) {
+		  } // Malformed character
+		  while (extras-->0) {
+		    Tmp += (*Input)[++i];
+		  }
+		}
+
+		CurSymbol = TextMap.Get(Tmp, &KeyIsPrefix);
+
+		if (KeyIsPrefix) {
+			CurSymbol = 0;
+			for (; i<Input->size(); i++) {
+
+				Tmp += (*Input)[i];
+
+				TmpSymbol = TextMap.Get(Tmp, &KeyIsPrefix);
+				if (TmpSymbol>0) {
+					CurSymbol = TmpSymbol;
+				}
+				if (!KeyIsPrefix) {
+					if (CurSymbol!=0) {
+						Symbols->push_back(CurSymbol);
+					} else {
+						i -= Tmp.size()-1;
+						//Tmp.erase(Tmp.begin(), Tmp.end());
+						Tmp = "";
+					}
+					break;
+				}
+			}
+		} else {
+			if (CurSymbol!=0)
+				Symbols->push_back(CurSymbol);
+		}
+	}
+
+	if (IsMore)
+		if (KeyIsPrefix)
+			*Input = Tmp;
+		else
+			*Input = "";
+	else
+		if (KeyIsPrefix)
+			Symbols->push_back(CurSymbol);
+}
+
+
+// add single char to the character set
+void CAlphabet::AddChar(const string NewCharacter, const string Display, int Colour, const string Foreground)
+{
+	m_Characters.push_back(NewCharacter);
+	m_Display.push_back(Display);
+	m_Colours.push_back(Colour);
+	m_Foreground.push_back(Foreground);
+
+	symbol ThisSymbol = m_Characters.size()-1;
+	TextMap.Add(NewCharacter, ThisSymbol);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+void CAlphabet::AddParagraphSymbol(const string NewCharacter, const string Display, int Colour, const string Foreground)
+{
+	AddChar(NewCharacter,Display,Colour,Foreground);
+	m_ParagraphSymbol = GetNumberSymbols()-1;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+void CAlphabet::AddSpaceSymbol(const string NewCharacter, const string Display, int Colour, const string Foreground)
+{
+	AddChar(NewCharacter,Display,Colour,Foreground);
+	m_SpaceSymbol = GetNumberSymbols()-1;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+void CAlphabet::AddControlSymbol(const string NewCharacter, const string Display, int Colour, const string Foreground)
+{
+	AddChar(NewCharacter,Display,Colour,Foreground);
+	m_ControlSymbol = GetNumberSymbols()-1;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// diagnostic dump of character set
+
+void CAlphabet::Trace() const 
+{
+	unsigned int i;
+	DASHER_TRACEOUTPUT("GetNumberSymbols() %d\n",GetNumberSymbols());
+	DASHER_TRACEOUTPUT("GetNumberTextSymbols() %d\n",GetNumberTextSymbols());
+	
+	int iGroup=0;
+	for (i=0;i<m_Characters.size();i++) 
+	{
+		if (iGroup < m_iGroups  && i==m_GroupStart[iGroup])
+		{
+			DASHER_TRACEOUTPUT("Group %d '%s'\n", iGroup, m_GroupLabel[iGroup].c_str());
+		}
+		if (iGroup < m_iGroups && i==m_GroupEnd[iGroup])
+		{
+			DASHER_TRACEOUTPUT("--------\n");
+			iGroup++;
+		}
+
+		DASHER_TRACEOUTPUT("Symbol %d Character:'%s' Display:'%s'\n",i,m_Characters[i].c_str(),m_Display[i].c_str());
+
+	}
+
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+int CAlphabet::GetTextColour(symbol Symbol)
+{
+  std::string TextColour=m_Foreground[Symbol];
+  if (TextColour !=  std::string("") ) {
+    return atoi(TextColour.c_str());
+  } else {
+    return 4;
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+CAlphabet::CGroupAdder* CAlphabet::GetGroupAdder(int iColour, const std::string& strLabel)
+{
+	return new CGroupAdder(*this, iColour, strLabel);
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+CAlphabet::CGroupAdder::CGroupAdder(CAlphabet& alphabet, int iColour, std::string strLabel)
+: m_Alphabet(alphabet)
+{
+	m_Alphabet.m_iGroups++;
+	m_Alphabet.m_GroupColour.push_back(iColour);
+	m_Alphabet.m_GroupLabel.push_back(strLabel);
+	m_Alphabet.m_GroupStart.push_back( m_Alphabet.GetNumberSymbols() ) ;
+
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+CAlphabet::CGroupAdder::~CGroupAdder()
+{
+	m_Alphabet.m_GroupEnd.push_back( m_Alphabet.GetNumberSymbols() ) ;
+
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+void CAlphabet::CGroupAdder::AddChar(const std::string NewCharacter, const std::string Display, int Colour, const std::string Foreground)
+{
+	m_Alphabet.AddChar(NewCharacter,Display,Colour,Foreground);
+
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
---
+++ Src/DasherCore/Alphabet/Alphabet.h
@@ -0,0 +1,160 @@
+// Alphabet.h
+//
+/////////////////////////////////////////////////////////////////////////////
+// Alphabet.h
+//
+/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2001-2002 David Ward
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef __DASHER_ALPHABET_H__
+#define __DASHER_ALPHABET_H__
+
+#include "../DasherTypes.h"
+
+
+#include <vector>
+#include "AlphabetMap.h"
+#include "AlphIO.h"
+
+
+namespace Dasher
+{
+
+class CAlphabet
+{
+public:
+
+	CAlphabet();
+
+	CAlphabet(const CAlphIO::AlphInfo& AlphInfo);
+
+	~CAlphabet() {};
+	
+	// Return size of alphabet, including control symbols
+	int GetNumberSymbols() const { return m_Characters.size();}        // return size of alphabet
+	
+	// Return number of text symbols
+	int GetNumberTextSymbols() const { return m_Characters.size()-1;}   
+	
+	Opts::ScreenOrientations GetOrientation() {return m_Orientation;}
+	Opts::AlphabetTypes GetType() {return m_DefaultEncoding;}
+	std::string& GetTrainingFile() {return m_TrainingFile;}
+	std::string& GetPalette() {return m_DefaultPalette;}
+	
+	symbol GetParagraphSymbol() const;
+	symbol GetSpaceSymbol() const;
+	symbol GetControlSymbol() const;
+	
+	const std::string& GetDisplayText(symbol i) const {return m_Display[i];} // return display string for i'th symbol
+	const std::string& GetText(symbol i) const {return m_Characters[i];}     // return string for i'th symbol
+	int GetColour(symbol i) const {return m_Colours[i];}     // return the colour for i'th symbol
+	int GetGroupColour(int i) const {return m_GroupColour[i];} // return the colour for i'th group
+	std::string GetGroupLabel(int i) const {return m_GroupLabel[i];}
+	int GetTextColour(symbol i);      // return the foreground colour for i'th symbol
+	const std::string& GetForeground(symbol i) const {return m_Foreground[i];} // return the foreground colour for i'th symbol
+
+	int GetGroupCount() const {return m_iGroups;}
+	int GetGroupStart(int i) const {return m_GroupStart[i];}
+	int GetGroupEnd(int i) const {return m_GroupEnd[i];}
+
+	//int get_group(symbol i) const {return m_Group[i];}                // return group membership of i'th symbol
+	
+	// Fills Symbols with the symbols corresponding to Input. {{{ Note that this
+	// is not necessarily reversible by repeated use of GetText. Some text
+	// may not be recognised and so discarded. If IsMore is true then Input
+	// is truncated to any final characters that were not used due to ambiguous
+	// continuation. If IsMore is false Input is assumed to be all the available
+	// text and so a symbol will be returned for a final "a" even if "ae" is
+	// defined as its own symbol. }}}
+	void GetSymbols(std::vector<symbol>* Symbols, std::string* Input, bool IsMore) const;
+	
+	void Trace() const; // diagnostic
+	
+
+	class CGroupAdder
+	{
+	public:
+		CGroupAdder(CAlphabet& alphabet, int iColour, std::string strLabel);
+		~CGroupAdder();
+		void AddChar(const std::string NewCharacter, const std::string Display, int Colour, const std::string Foreground);
+
+	private:
+		friend CAlphabet;
+
+		CAlphabet& m_Alphabet;
+
+	};
+
+
+	CGroupAdder* GetGroupAdder(int iColour, const std::string& strLabel);
+
+	// Add the characters that can appear in Nodes
+	void AddChar(std::string NewCharacter, std::string Display, int Colour, std::string Foreground); // add single char to the alphabet
+
+
+	// Alphabet language parameters
+	void AddParagraphSymbol(std::string NewCharacter, std::string Display, int Colour, std::string Foreground);
+	void AddSpaceSymbol(std::string NewCharacter, std::string Display, int Colour, std::string Foreground);  
+	void AddControlSymbol(std::string NewCharacter, std::string Display, int Colour, std::string Foreground);
+	
+	void SetOrientation(Opts::ScreenOrientations Orientation) {m_Orientation=Orientation;}
+	void SetLanguage(Opts::AlphabetTypes Group) {m_DefaultEncoding=Group;}
+	void SetTrainingFile(std::string TrainingFile) {m_TrainingFile=TrainingFile;}
+	void SetPalette(std::string Palette) {m_DefaultPalette=Palette;}
+
+private:
+
+	Opts::AlphabetTypes m_DefaultEncoding;
+	Opts::ScreenOrientations m_Orientation;
+	symbol m_ParagraphSymbol;
+	symbol m_SpaceSymbol;
+	symbol m_ControlSymbol;
+	std::string m_TrainingFile;
+	std::string m_DefaultPalette;
+
+	std::vector<std::string> m_Characters; // stores the characters
+	std::vector<std::string> m_Display;    // stores how the characters are visually represented in the Dasher nodes
+	std::vector<int> m_Colours;    // stores the colour of the characters
+	std::vector<std::string> m_Foreground; // stores the colour of the character foreground
+
+	int m_iGroups;                          // number of groups
+
+	std::vector<int> m_GroupStart;     // stores the group start index
+	std::vector<int> m_GroupEnd;     // stores the group end index (1 past the last)
+	std::vector<int> m_GroupColour; // stores the colour of the group
+	std::vector<std::string> m_GroupLabel;
+
+	alphabet_map TextMap;
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+
+inline symbol CAlphabet::GetParagraphSymbol() const 
+{
+	return m_ParagraphSymbol;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+inline symbol CAlphabet::GetSpaceSymbol() const 
+{
+	return m_SpaceSymbol;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+inline symbol CAlphabet::GetControlSymbol() const 
+{
+ 	return m_ControlSymbol;
+}
+
+
+} // end namespace dasher
+
+/////////////////////////////////////////////////////////////////////////////
+
+#endif // ifndef __DASHER_ALPHABET_H__
