80f76870fa535388da1d8ef9e07c0d8c0883850a
Chris Ball <cball@src.gnome.org>
2005-06-09 15:49:48 +0000

    DrawMouseLine fixes from Brian Williams.  Get the line's origin to be on the crosshair and correct in all orientations.

--- Src/DasherCore/DasherViewSquare.cpp
+++ Src/DasherCore/DasherViewSquare.cpp
@@ -332,48 +332,36 @@ void CDasherViewSquare::DrawMouse(screenint mousex, screenint mousey)
 
 void CDasherViewSquare::DrawMouseLine(screenint mousex, screenint mousey)
 {
-        if (DasherModel().Dimensions()==true || DasherModel().Eyetracker()==true) {
-  
-	  screenint Swapper;
-	
-	  myint dasherx,dashery;
-	  screen2dasher(mousex,mousey,&dasherx,&dashery);
-	  mousex=dasherx2screen(dasherx);
-	  mousey=dashery2screen(dashery);
-	  switch (ScreenOrientation) {
-	  case (LeftToRight):
-	    break;
-	  case (RightToLeft):
-	    mousex = Screen().GetWidth() - mousex;
-	    break;
-	  case (TopToBottom):
-	    Swapper = ( mousex * Screen().GetHeight()) / Screen().GetWidth();
-	    mousex = (mousey  * Screen().GetWidth()) / Screen().GetHeight();
-	    mousey = Swapper;
-	    break;
-	  case (BottomToTop):
-	    // Note rotation by 90 degrees not reversible like others
-	    Swapper = Screen().GetHeight() - ( mousex * Screen().GetHeight()) / Screen().GetWidth();
-	    mousex = (mousey  * Screen().GetWidth()) / Screen().GetHeight();
-	    mousey = Swapper;
-	    break;
-	  default:
-	    break;
-	  }
-	}
+    if (DasherModel().Eyetracker()==true) { 
+        UnMapScreen(&mousex, &mousey); 
+    } 
+ 
+    if (DasherModel().Dimensions()==true || DasherModel().Eyetracker()==true) { 
+     screenint Swapper; 
+       myint dasherx,dashery; 
+ 
+      screen2dasher(mousex,mousey,&dasherx,&dashery); 
+      mousex=dasherx2screen(dasherx); 
+      mousey=dashery2screen(dashery); 
+      MapScreen(&mousex, &mousey); 
+    } 
+ 
+    CDasherScreen::point mouseline[2]; 
+ 
+    mouseline[0].x=dasherx2screen(DasherModel().DasherOX()); 
+    mouseline[0].y=CanvasY/2; 
+    mouseline[1].x=mousex; 
+    mouseline[1].y=mousey; 
+ 
+    // Brian W. 7/6/05 - Ensuring line starts at crosshair origin 
+    MapScreen(&mouseline[0].x, &mouseline[0].y); 
+ 
+    if (ColourMode==true) { 
+      Screen().Polyline(mouseline,2,1); 
+    } else { 
+      Screen().Polyline(mouseline,2); 
+    } 
 
-	CDasherScreen::point mouseline[2];
-
-	mouseline[0].x=dasherx2screen(DasherModel().DasherOX());
-	mouseline[0].y=CanvasY/2;
-	mouseline[1].x=mousex;
-	mouseline[1].y=mousey;	  	
-
-	if (ColourMode==true) {
-	  Screen().Polyline(mouseline,2,1);
-	} else {
-	  Screen().Polyline(mouseline,2);
-	}
 }
 
 /////////////////////////////////////////////////////////////////////////////
