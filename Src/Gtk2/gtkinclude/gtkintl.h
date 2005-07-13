af698b668a7a457dc2ee8eaabdb1c79470c5d593
Chris Ball <cball@src.gnome.org>
2005-07-13 22:59:14 +0000

    try to add missing text view files

---
+++ Src/Gtk2/gtkinclude/gtkintl.h
@@ -0,0 +1,15 @@
+#ifndef __GTKINTL_H__
+#define __GTKINTL_H__
+
+// XXXfrederik
+//#include "config.h"
+
+#include <glib/gi18n-lib.h>
+
+#ifdef ENABLE_NLS
+#define P_(String) dgettext(GETTEXT_PACKAGE "-properties",String)
+#else 
+#define P_(String) (String)
+#endif
+
+#endif
