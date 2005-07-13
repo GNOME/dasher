44825fdf11a58a267fa56a03d22d867db1aee508
Phil Cowans <pcowans@src.gnome.org>
2005-07-13 15:29:35 +0000

    no more settings default method

--- Src/DasherCore/Win32/DasherInterface.cpp
+++ Src/DasherCore/Win32/DasherInterface.cpp
@@ -3,7 +3,7 @@
 
 CDasherInterface::CDasherInterface(void) {
 	m_pSettingsStore = new CWinOptions( "Inference Group", "Dasher3", m_pEventHandler );
-	SettingsDefaults( m_pSettingsStore );
+	//SettingsDefaults( m_pSettingsStore );
     CDasherSettingsInterface::SetInterface(this);
 }
 
