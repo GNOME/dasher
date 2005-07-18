d46f91abc06192af6c2592cd23e774068130d8d9
Phil Cowans <pcowans@src.gnome.org>
2005-07-18 21:10:05 +0000

    Added doxygen configuration file
    * Added doxygen configuration file

--- Src/DasherCore/DasherView.cpp
+++ Src/DasherCore/DasherView.cpp
@@ -15,101 +15,92 @@ using namespace Dasher;
 
 /////////////////////////////////////////////////////////////////////////////
 
-CDasherView::CDasherView(CEventHandler *pEventHandler, CSettingsStore *pSettingsStore, CDasherScreen* DasherScreen, CDasherModel& DasherModel)
-  : CDasherComponent( pEventHandler, pSettingsStore ), m_pScreen(DasherScreen), m_DasherModel(DasherModel),
-    m_pInput(0)
-{
+CDasherView::CDasherView(CEventHandler *pEventHandler, CSettingsStore *pSettingsStore, CDasherScreen *DasherScreen, CDasherModel &DasherModel)
+:CDasherComponent(pEventHandler, pSettingsStore), m_pScreen(DasherScreen), m_DasherModel(DasherModel), m_pInput(0) {
 
 }
 
-
-
-void CDasherView::HandleEvent( Dasher::CEvent *pEvent ) {
-  if( pEvent->m_iEventType == 1 ) {
-    Dasher::CParameterNotificationEvent	*pEvt( static_cast< Dasher::CParameterNotificationEvent* >( pEvent ));
-    switch( pEvt->m_iParameter ) {
+void CDasherView::HandleEvent(Dasher::CEvent *pEvent) {
+  if(pEvent->m_iEventType == 1) {
+    Dasher::CParameterNotificationEvent * pEvt(static_cast < Dasher::CParameterNotificationEvent * >(pEvent));
+    switch (pEvt->m_iParameter) {
     default:
       break;
     }
   }
 };
 
-
 /////////////////////////////////////////////////////////////////////////////
 
-void CDasherView::ChangeScreen(CDasherScreen* NewScreen)
-{
-  m_pScreen=NewScreen;
+void CDasherView::ChangeScreen(CDasherScreen *NewScreen) {
+  m_pScreen = NewScreen;
 }
 
 /////////////////////////////////////////////////////////////////////////////
 
 //void CDasherView::ChangeOrientation(Dasher::Opts::ScreenOrientations Orientation)
 //{
-//	ScreenOrientation = Orientation;
+//      ScreenOrientation = Orientation;
 //}
 
-void CDasherView::DrawMousePosBox()
-{
+void CDasherView::DrawMousePosBox() {
   int iHeight = Screen().GetHeight();
   int iWidth = Screen().GetWidth();
-  
-  //	DASHER_TRACEOUTPUT("which %d\n",iWhich);
+
+  //    DASHER_TRACEOUTPUT("which %d\n",iWhich);
   int iMousePosDist = GetLongParameter(LP_MOUSEPOSDIST);
   int iDrawMousePosBox = GetLongParameter(LP_MOUSE_POS_BOX);
-  
+
   //  std::cout << "iDrawMousePosBox: " << iDrawMousePosBox << std::endl;
-  
-  switch (iDrawMousePosBox) 
-    {
-    case 1:
-      Screen().DrawRectangle(0,iHeight/2-iMousePosDist+50,iWidth,iHeight/2-iMousePosDist-50,119, Opts::Nodes1);
-      break;	
-    case 2:
-      Screen().DrawRectangle(0,iHeight/2+iMousePosDist+50,iWidth,iHeight/2+iMousePosDist-50,120, Opts::Nodes1);
-      break;
-    default:
-      //      DASHER_ASSERT(0);
-      break;
-    }
+
+  switch (iDrawMousePosBox) {
+  case 1:
+    Screen().DrawRectangle(0, iHeight / 2 - iMousePosDist + 50, iWidth, iHeight / 2 - iMousePosDist - 50, 119, Opts::Nodes1);
+    break;
+  case 2:
+    Screen().DrawRectangle(0, iHeight / 2 + iMousePosDist + 50, iWidth, iHeight / 2 + iMousePosDist - 50, 120, Opts::Nodes1);
+    break;
+  default:
+    //      DASHER_ASSERT(0);
+    break;
+  }
 }
 
 /////////////////////////////////////////////////////////////////////////////
 
-bool CDasherView::Render(int iMouseX, int iMouseY, bool bRedrawDisplay)
-{
+bool CDasherView::Render(int iMouseX, int iMouseY, bool bRedrawDisplay) {
 
-  bool bDidSomething(false); // Have we actually done any drawing - no
-			     // point updating the display if we
-			     // didn't
+  bool bDidSomething(false);    // Have we actually done any drawing - no
+  // point updating the display if we
+  // didn't
 
-  if( bRedrawDisplay ) {
-    Screen().SendMarker(0); // Start of 'dasher field'
+  if(bRedrawDisplay) {
+    Screen().SendMarker(0);     // Start of 'dasher field'
     RenderNodes();
     bDidSomething = true;
   }
 
-  Screen().SendMarker(1); // Start of 'decoration'
-  
-  if (GetBoolParameter(BP_DRAW_MOUSE)) {
+  Screen().SendMarker(1);       // Start of 'decoration'
+
+  if(GetBoolParameter(BP_DRAW_MOUSE)) {
     DrawMouse(iMouseX, iMouseY);
     bDidSomething = true;
   }
-  if (GetBoolParameter(BP_DRAW_MOUSE_LINE)) {
+  if(GetBoolParameter(BP_DRAW_MOUSE_LINE)) {
     DrawMouseLine(iMouseX, iMouseY);
     bDidSomething = true;
   }
-  if (GetBoolParameter(BP_KEYBOARD_MODE)) {
+  if(GetBoolParameter(BP_KEYBOARD_MODE)) {
     DrawKeyboard();
     bDidSomething = true;
 
   }
-  if (GetLongParameter(LP_MOUSE_POS_BOX) !=-1) {
+  if(GetLongParameter(LP_MOUSE_POS_BOX) != -1) {
     DrawMousePosBox();
     bDidSomething = true;
   }
 
-  if (GetBoolParameter(BP_GAME_MODE) != 0) {
+  if(GetBoolParameter(BP_GAME_MODE) != 0) {
     DrawGameModePointer();
     bDidSomething = true;
   }
@@ -118,23 +109,20 @@ bool CDasherView::Render(int iMouseX, int iMouseY, bool bRedrawDisplay)
 
 }
 
-
 /////////////////////////////////////////////////////////////////////////////
 
-void CDasherView::Render()
-{
+void CDasherView::Render() {
 
   // FIXME - when does this get called?
 
   Screen().SendMarker(0);
 
   RenderNodes();
-  
+
   Screen().SendMarker(1);
-  
-  if (GetBoolParameter(BP_KEYBOARD_MODE)) 
+
+  if(GetBoolParameter(BP_KEYBOARD_MODE))
     DrawKeyboard();
-  if (GetLongParameter(LP_MOUSE_POS_BOX) !=-1)
+  if(GetLongParameter(LP_MOUSE_POS_BOX) != -1)
     DrawMousePosBox();
 }
-
