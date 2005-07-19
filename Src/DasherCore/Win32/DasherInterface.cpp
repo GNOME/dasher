8309ab1bcf86d1689481b5bd22c08369c5a68e1f
Brian Williams <brianw@src.gnome.org>
2005-07-19 16:48:00 +0000

    Added method

--- Src/DasherCore/Win32/DasherInterface.cpp
+++ Src/DasherCore/Win32/DasherInterface.cpp
@@ -3,13 +3,22 @@
 
 CDasherInterface::CDasherInterface(void) {
 	m_pSettingsStore = new CWinOptions( "Inference Group", "Dasher3", m_pEventHandler );
-	//SettingsDefaults( m_pSettingsStore );
     CDasherSettingsInterface::SetInterface(this);
 
-ChangeAlphabet( GetStringParameter(SP_ALPHABET_ID) );
+    ChangeAlphabet( GetStringParameter(SP_ALPHABET_ID) );
 
 }
 
 CDasherInterface::~CDasherInterface(void)
 {
+    delete m_pSettingsStore;
+}
+
+CSettingsStore* CDasherInterface::CreateSettingsStore(void)
+{
+    if(m_pSettingsStore)
+        return m_pSettingsStore;
+
+    m_pSettingsStore = new CWinOptions( "Inference Group", "Dasher3", m_pEventHandler );
+    return m_pSettingsStore;
 }
