99946953ab4fe4f23211b4925b5870018f36c48a
Phil Cowans <pcowans@src.gnome.org>
2006-04-10 18:36:29 +0000

    Added missing files (fixes #337309)
    * Added missing files (fixes #337309)

---
+++ Src/DasherCore/StylusFilter.h
@@ -0,0 +1,14 @@
+#ifndef __STYLUS_FILTER_H__
+#define __STYLUS_FILTER_H__
+
+#include "DefaultFilter.h"
+
+class CStylusFilter : public CDefaultFilter {
+ public:
+  CStylusFilter(Dasher::CEventHandler * pEventHandler, CSettingsStore *pSettingsStore, CDasherInterfaceBase *pInterface, CDasherModel *pDasherModel, long long int iID, const char *szName);
+
+  virtual void KeyDown(int iTime, int iId, CDasherModel *pModel);
+  virtual void KeyUp(int iTime, int iId, CDasherModel *pModel);
+};
+
+#endif
