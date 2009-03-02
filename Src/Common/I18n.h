200163000ca94e1ea951975b94907b0e2b495a1f
Patrick Welche <pwelche@src.gnome.org>
2009-03-02 17:07:20 +0000

    The N_ macro is defined in /usr/include/glib-2.0/glib/gi18n.h Those configure flags revealed another missing part to the move to gvfs.
    svn path=/trunk/; revision=3635

--- Src/Common/I18n.h
+++ Src/Common/I18n.h
@@ -24,16 +24,22 @@
 #if defined(DASHER_WIN32) || defined(__APPLE__)
 
 #define _(szText) szText
+#define N_(szText) (szText)
 
 #else
 
 #include <libintl.h>
-// Attempt to get rid of '"_" rededined' compiler warnings.  I'm not sure the
+// Attempt to get rid of '"_" redefined' compiler warnings.  I'm not sure the
 // proper way to verify that gnome i18n support is present, so if you have a
 // better idea...
 #ifndef _
 #define _(szText) gettext(szText)
 #endif
+
+#ifndef N_
+#define N_(szText) (szText)
+#endif
+
 #endif
 
 #endif
