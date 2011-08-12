1f9908c9e0ec0eb0a544803179dac91c3bc7b340
Patrick Welche <prlw1@cam.ac.uk>
2011-08-12 17:28:59 +0100

    g/c more commented out code (dasher.h looks trivial now)

--- Src/Gtk2/dasher_editor_internal.h
+++ Src/Gtk2/dasher_editor_internal.h
@@ -49,35 +49,6 @@ void dasher_editor_internal_initialise(DasherEditorInternal *pSelf, DasherAppSet
 
 GType dasher_editor_internal_get_type();
 
-/* void dasher_editor_internal_handle_font(DasherEditorInternal *pSelf, const gchar *szFont); */
-
-/* /\* To be obsoleted by movement to GTK buffers *\/ */
-/* void dasher_editor_internal_output(DasherEditorInternal *pSelf, const gchar *szText, int iOffset); */
-/* void dasher_editor_internal_delete(DasherEditorInternal *pSelf, int iLength, int iOffset); */
-/* const gchar *dasher_editor_internal_get_context(DasherEditorInternal *pSelf, int iOffset, int iLength); */
-/* gint dasher_editor_internal_get_offset(DasherEditorInternal *pSelf); */
-
-/* /\* Events proagated from main *\/ */
-/* void dasher_editor_internal_handle_stop(DasherEditorInternal *pSelf); */
-/* void dasher_editor_internal_handle_start(DasherEditorInternal *pSelf); */
-
-/* /\* Action related methods - TODO: a lot of this should be moved to dasher_main (eg action on stop etc) - that way we get a better level of abstraction, and can incorporate commands from other modules too. Actions should only be externally visible as a list of string commands*\/ */
-/* void dasher_editor_internal_action_button(DasherEditorInternal *pSelf, DasherAction *pAction); */
-/* void dasher_editor_internal_actions_start(DasherEditorInternal *pSelf); */
-/* bool dasher_editor_internal_actions_more(DasherEditorInternal *pSelf); */
-/* void dasher_editor_internal_actions_get_next(DasherEditorInternal *pSelf, const gchar **szName, gint *iID, gboolean *bShow, gboolean *bControl, gboolean *bAuto); */
-/* void dasher_editor_internal_action_set_show(DasherEditorInternal *pSelf, int iActionID, bool bValue); */
-/* void dasher_editor_internal_action_set_control(DasherEditorInternal *pSelf, int iActionID, bool bValue); */
-/* void dasher_editor_internal_action_set_auto(DasherEditorInternal *pSelf, int iActionID, bool bValue); */
-
-/* void dasher_editor_internal_grab_focus(DasherEditorInternal *pSelf); */
-
-/* /\* Todo: possibly tidy up the need to have this public (quit in dasher_main possibly too connected) *\/ */
-/* gboolean dasher_editor_internal_file_changed(DasherEditorInternal *pSelf); */
-/* const gchar *dasher_editor_internal_get_filename(DasherEditorInternal *pSelf); */
-
-/* const gchar *dasher_editor_internal_get_all_text(DasherEditorInternal *pSelf); */
-/* const gchar *dasher_editor_internal_get_new_text(DasherEditorInternal *pSelf); */
 G_END_DECLS
 
 #endif
