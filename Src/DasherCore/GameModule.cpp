GNOME/dasher

commit 1faa979ef3aa64a621f4a8f789766cc83b7214e0

Avoid max() and define myint abs(myint) to avoid gcc's
  template<typename _Tp>
    inline typename __gnu_cxx::__enable_if<__is_integer<_Tp>::__value,
                                           double>::__type
    abs(_Tp __x)
    { return __builtin_fabs(__x); }

Src/DasherCore/GameModule.cpp
@@ -85,8 +85,8 @@ void CGameModule::HandleEvent(const CEditEvent *evt) {
 void CGameModule::HandleEvent(CGameNodeDrawEvent *gmd) {
   //game nodes form a single chain, i.e. are strictly nested.
   // we want the coordinates of the smallest (innermost) one about which we are told
-  m_y1 = max(m_y1, gmd->m_y1);
-  m_y2 = min(m_y2, gmd->m_y2);
+  if (gmd->m_y1 > m_y1) m_y1 = gmd->m_y1;
+  if (gmd->m_y2 < m_y1) m_y2 = gmd->m_y2;
 }
 
 void CGameModule::HandleEvent(CDasherView *pView) {
@@ -112,6 +112,8 @@ void CGameModule::StartWriting(unsigned long lTime) {
     m_dSentenceStartNats = numeric_limits<double>::max();
   }
 }
+
+static myint abs(myint x) { return (x>0)?x:-x; }
 
 void CGameModule::DecorateView(unsigned long lTime, CDasherView *pView, CDasherModel *pModel) {
 

commit 6ff206b92bfbcaff01f245aafed4989ece86bc81

Fix typo introduced in 1faa979ef3aa64a621f4a8f789766cc83b7214e0

Src/DasherCore/GameModule.cpp
@@ -86,7 +86,7 @@ void CGameModule::HandleEvent(CGameNodeDrawEvent *gmd) {
   //game nodes form a single chain, i.e. are strictly nested.
   // we want the coordinates of the smallest (innermost) one about which we are told
   if (gmd->m_y1 > m_y1) m_y1 = gmd->m_y1;
-  if (gmd->m_y2 < m_y1) m_y2 = gmd->m_y2;
+  if (gmd->m_y2 < m_y2) m_y2 = gmd->m_y2;
 }
 
 void CGameModule::HandleEvent(CDasherView *pView) {