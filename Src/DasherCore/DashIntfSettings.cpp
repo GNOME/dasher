80430aeffccfff6b0b60271f72c573198ce18dc7
Patrick Welche <prlw1@cam.ac.uk>
2012-08-18 09:45:58 +0100

    Fix alphabet combo box
    Previous alphabets used are now remembered. (introduced in df5eebe2)

--- Src/DasherCore/DashIntfSettings.cpp
+++ Src/DasherCore/DashIntfSettings.cpp
@@ -28,5 +28,6 @@ void CDashIntfSettings::SetLongParameter(int iParameter, long lValue) {
 }
 
 void CDashIntfSettings::SetStringParameter(int iParameter, const string &strValue) {
+  CDasherInterfaceBase::PreSetNotify(iParameter, strValue);
   CDasherInterfaceBase::SetStringParameter(iParameter, strValue);
 }
