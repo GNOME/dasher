23a49b9818be7109c1e5554775da25bf3751330c
ipomoena <amajorek@google.com>
2015-10-14 16:34:35 -0700

    replaced for each with plain for loop. too new

--- Src/DasherCore/ControlManager.cpp
+++ Src/DasherCore/ControlManager.cpp
@@ -375,8 +375,8 @@ CControlBase::Action *CControlManager::parseAction(const XML_Char *name, const X
   // Key in actions map is name plus arguments in alphabetical order.
   key << name;
   if (!arguments.empty()) {
-    for each (auto arg in arguments) {
-      key << " " << arg.first << "=" << arg.second;
+    for (auto it = arguments.begin(); it != arguments.end(); ++it) {
+      key << " " << it->first << "=" << it->second;
     }
   }
   if (auto action = m_actions[key.str()])
